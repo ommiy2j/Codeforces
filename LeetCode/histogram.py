b02540b2c4f28a0a66e3de125167cb8d3be4fee1
omprakash <ommiy2j@gmail.com>
2021-01-12 22:16:41 +0530

    leetcode

--- LeetCode/histogram.py
+++ LeetCode/histogram.py
@@ -1,37 +1,40 @@
-l=[int(i) for i in input().split()]
-stack=[]
-i=0
-top=0
-maxA=0
-while(i<len(l)):
-    if len(stack)==0 or l[top]<=l[i]:
-        stack.append(i)
-        top=i
-        i+=1
-    else:
-        temp=stack[-1]
-        stack.pop()
-        if (len(stack)==0):
-            a=l[temp]*i
-        else:
-            a=l[temp]*(i-stack[-1]-1)
-            top-=1
-        if a>maxA:
-            maxA=a
-while(len(stack)!=0):
-    temp=stack[-1]
-    stack.pop()
-    if (len(stack)==0):
-        a=l[temp]*i
-        top-=1
-    else:
-        a=l[temp]*(i-stack[-1]-1)
-        top-=1
-    if a>maxA:
-        maxA=a
-print(maxA)
+class Solution:
+    def largestRectangleArea(self, l: List[int]) -> int:
+        stack=[]
+        i=0
+        top=0
+        maxA=0
+        while(i<len(l)):
+            if len(stack)==0 or l[stack[-1]]<=l[i]:
+                stack.append(i)
+                top=i
+                i+=1
+            else:
+                temp=stack[-1]
+                stack.pop()
+                if (len(stack)==0):
+                    a=l[temp]*i
+                else:
+                    a=l[temp]*(i-stack[-1]-1)
+                    top-=1
+                if a>maxA:
+                    maxA=a
+        while(len(stack)!=0):
+            temp=stack[-1]
+            stack.pop()
+            if (len(stack)==0):
+                a=l[temp]*i
+                top-=1
+            else:
+                a=l[temp]*(i-stack[-1]-1)
+                top-=1
+            if a>maxA:
+                maxA=a
+        return maxA
             
     
 
+        
+        
         
         
